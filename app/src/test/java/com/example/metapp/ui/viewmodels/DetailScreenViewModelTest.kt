da16d8eddf7be0e9f121a7168549386bfdf2fe35
PrimoDev23 <lexx.ps2711@gmail.com>
2024-01-12 12:04:42 +0100

    Add unit tests for ViewModels

---
+++ app/src/test/java/com/example/metapp/ui/viewmodels/DetailScreenViewModelTest.kt
@@ -0,0 +1,56 @@
+package com.example.metapp.ui.viewmodels
+
+import androidx.lifecycle.SavedStateHandle
+import app.cash.turbine.test
+import com.example.metapp.BaseCoroutineTest
+import com.example.metapp.domain.models.DetailData
+import com.example.metapp.domain.usecases.interfaces.GetDetailByIdUseCase
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class DetailScreenViewModelTest : BaseCoroutineTest() {
+
+    private fun buildSavedStateHandle(id: Int) = SavedStateHandle().apply {
+        this["id"] = id
+    }
+
+    @Test
+    fun init() = runTest {
+        val id = 0
+        val savedStateHandle = buildSavedStateHandle(id)
+        val getDetailByIdUseCase = mockk<GetDetailByIdUseCase>()
+
+        val detailData = DetailData(
+            primaryImage = "",
+            additionalImages = emptyList(),
+            isHighlight = true,
+            title = "Mona Lisa",
+            department = "Art",
+            objectUrl = "https://www.google.com"
+        )
+
+        coEvery { getDetailByIdUseCase(id) } returns detailData
+
+        val viewModel = DetailScreenViewModel(
+            savedStateHandle = savedStateHandle,
+            getDetailByIdUseCase = getDetailByIdUseCase
+        )
+
+        viewModel.state.test {
+            var state = awaitItem()
+
+            assertEquals(null, state.data)
+
+            state = awaitItem()
+
+            assertEquals(detailData, state.data)
+
+            coVerify { getDetailByIdUseCase(id) }
+        }
+    }
+
+}
---
+++ app/src/test/java/com/example/metapp/ui/viewmodels/SearchScreenViewModelTest.kt
@@ -0,0 +1,133 @@
+package com.example.metapp.ui.viewmodels
+
+import app.cash.turbine.test
+import com.example.metapp.BaseCoroutineTest
+import com.example.metapp.domain.models.SearchResult
+import com.example.metapp.domain.usecases.interfaces.SearchUseCase
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class SearchScreenViewModelTest : BaseCoroutineTest() {
+
+    @Test
+    fun init() = runTest {
+        val viewModel = SearchScreenViewModel(
+            searchUseCase = mockk()
+        )
+
+        viewModel.state.test {
+            val state = awaitItem()
+
+            assertEquals(SearchScreenContentType.EMPTY, state.contentType)
+            assertTrue(state.searchTerm.isEmpty())
+            assertEquals(
+                SearchResult(ids = emptyList()),
+                state.searchResult
+            )
+        }
+    }
+
+    @Test
+    fun `onSearchTermChanged - has entries`() = runTest {
+        val searchUseCase = mockk<SearchUseCase>()
+
+        val searchResult = SearchResult(
+            ids = listOf(0, 1, 2, 3)
+        )
+
+        coEvery { searchUseCase(any()) } returns searchResult
+
+        val viewModel = SearchScreenViewModel(
+            searchUseCase = searchUseCase
+        )
+
+        viewModel.state.test {
+            skipItems(1)
+
+            val searchTerm = "Test"
+            viewModel.onSearchTermChanged(searchTerm)
+
+            var state = awaitItem()
+
+            assertEquals(searchTerm, state.searchTerm)
+            assertEquals(SearchScreenContentType.LOADING, state.contentType)
+
+            state = awaitItem()
+
+            assertEquals(searchResult, state.searchResult)
+            assertEquals(SearchScreenContentType.RESULTS, state.contentType)
+
+            coVerify { searchUseCase(searchTerm) }
+        }
+    }
+
+    @Test
+    fun `onSearchTermChanged - no entries`() = runTest {
+        val searchUseCase = mockk<SearchUseCase>()
+
+        val searchResult = SearchResult(ids = emptyList())
+
+        coEvery { searchUseCase(any()) } returns searchResult
+
+        val viewModel = SearchScreenViewModel(
+            searchUseCase = searchUseCase
+        )
+
+        viewModel.state.test {
+            skipItems(1)
+
+            val searchTerm = "Test"
+            viewModel.onSearchTermChanged(searchTerm)
+
+            var state = awaitItem()
+
+            assertEquals(searchTerm, state.searchTerm)
+            assertEquals(SearchScreenContentType.LOADING, state.contentType)
+
+            state = awaitItem()
+
+            assertEquals(searchResult, state.searchResult)
+            assertEquals(SearchScreenContentType.EMPTY, state.contentType)
+
+            coVerify { searchUseCase(searchTerm) }
+        }
+    }
+
+    @Test
+    fun `onSearchTermChanged - clear term`() = runTest {
+        val searchUseCase = mockk<SearchUseCase>()
+
+        val searchResult = SearchResult(
+            ids = listOf(0, 1, 2, 3)
+        )
+
+        coEvery { searchUseCase(any()) } returns searchResult
+
+        val viewModel = SearchScreenViewModel(
+            searchUseCase = searchUseCase
+        )
+
+        viewModel.state.test {
+            viewModel.onSearchTermChanged("Test")
+
+            skipItems(3)
+
+            val emptyTerm = ""
+            viewModel.onSearchTermChanged(emptyTerm)
+
+            val state = awaitItem()
+
+            assertEquals(emptyTerm, state.searchTerm)
+            assertEquals(SearchScreenContentType.EMPTY, state.contentType)
+            assertTrue(state.searchResult.ids.isEmpty())
+
+            coVerify(inverse = true) { searchUseCase(emptyTerm) }
+        }
+    }
+
+}
